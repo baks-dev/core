e7cd020a07850c06ac31fcb1bf5e72f79086b9e1
kingstar <admin@baks.dev>
2024-06-11 12:52:11 +0300

    Baks Development

---
+++ Listeners/Event/CryptKeyListener.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ *  Copyright 2023.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Core\Listeners\Event;
+
+use BaksDev\Core\Type\Locale\Locale;
+use BaksDev\Core\Type\Locale\Locales\LocaleCollection;
+use Symfony\Component\Console\Event\ConsoleCommandEvent;
+use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Twig\Environment;
+
+#[AsEventListener(event: RequestEvent::class, priority: 1)]
+final class CryptKeyListener
+{
+    private LocaleCollection $collection;
+
+    public function __construct(LocaleCollection $collection)
+    {
+        $this->collection = $collection;
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $this->collection->cases();
+    }
+
+    public function onConsoleCommand(ConsoleCommandEvent $event): void
+    {
+        $this->collection->cases();
+    }
+}
