b8bd64418ec63aa2c4eca0bdf6908f850a98abe8
kingstar <admin@baks.dev>
2024-12-06 18:38:13 +0300

    Baks Development

--- Resources/assets/js/scroll.js
+++ Resources/assets/js/scroll.js
@@ -1,3 +1,25 @@
+/*
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
 const scrollElements = document.querySelectorAll('[data-scroll]');
 
 scrollElements.forEach(element =>
@@ -51,13 +73,16 @@ function scrollToElement(elementId, position = 'center')
 
     switch(position)
     {
-        case 'top':
+        case 'bottom':
             scrollTop = element.offsetTop;
             break;
-        case 'bottom':
+
+        case 'top':
             scrollTop = element.offsetTop + element.offsetHeight;
             break;
+
         case 'center':
+
         default:
             scrollTop = element.offsetTop + (element.offsetHeight / 2) - (window.innerHeight / 2);
             break;
