b22c04591cdb4b50338fc9793857e2c851ef4271
MichelAngelo <baksdevelopmen@gmail.com>
2025-10-09 00:57:09 +0300

    BaksDevelopment

--- Command/DevelopmentCommand.php
+++ Command/DevelopmentCommand.php
@@ -30,6 +30,9 @@ use BaksDev\Core\Messenger\MessageDispatchInterface;
 use BaksDev\Orders\Order\Messenger\OrderMessage;
 use BaksDev\Orders\Order\Type\Event\OrderEventUid;
 use BaksDev\Orders\Order\Type\Id\OrderUid;
+use BaksDev\Products\Stocks\Messenger\ProductStockMessage;
+use BaksDev\Products\Stocks\Type\Event\ProductStockEventUid;
+use BaksDev\Products\Stocks\Type\Id\ProductStockUid;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
@@ -52,11 +55,19 @@ class DevelopmentCommand extends Command
     {
         $io = new SymfonyStyle($input, $output);
 
-        $this->MessageDispatch->dispatch($OrderMessage = new OrderMessage(
+        $ProductStockMessage = new ProductStockMessage(
+            new ProductStockUid('0199baae-ee55-756e-a231-dd02dc324709'),
+            new ProductStockEventUid('0199baae-ee55-756e-a231-dd02dd12308f'),
+            null,
+        );
+
+        $OrderMessage = new OrderMessage(
             new OrderUid('01992feb-aaf0-7e42-a242-c4d6de87db76'),
             new OrderEventUid('01992fef-5037-7de4-a561-963a95d45750'),
             new OrderEventUid('01992fed-96c0-7e8b-a048-ac743f3070c5'),
-        ));
+        );
+
+        $this->MessageDispatch->dispatch($ProductStockMessage);
 
         $io->success('baks:development');
         return Command::SUCCESS;
