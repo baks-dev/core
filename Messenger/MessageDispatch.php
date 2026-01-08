9254beae5b56b2faafc44daaed485a7bc72a2ee8
MichelAngelo <baksdevelopmen@gmail.com>
2026-01-08 17:28:16 +0300

    BaksDevelopment

--- Messenger/MessageDispatch.php
+++ Messenger/MessageDispatch.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -63,7 +63,7 @@ final class MessageDispatch implements MessageDispatchInterface
         if($transport)
         {
             /* Чистим кеш модуля (транспорта) */
-            $cache = $this->cache->init($transport);
+            $cache = $this->cache->init(str_replace('-low', '', $transport));
             $cache->clear();
 
             new FilesystemAdapter($transport)->clear();
